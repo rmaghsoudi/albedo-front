07c4683b15a4a38d3f4c15ce590111b09b4edcd5
Romy Maghsoudi <romy_mag@outlook.com>
2019-12-11 10:50:20 -0500

    base auth0 setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./components/Dashboard";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Dashboard} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+
+  return(
+    <div>
+      I'm the dashboard
+    </div>
+  )
+
+}
+
+export default Dashboard
